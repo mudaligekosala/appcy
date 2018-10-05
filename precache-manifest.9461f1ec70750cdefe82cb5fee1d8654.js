eaa7b9a50a6ea2378da6566f489f15ab9373cebb
mudalige <mudaligekosala@gmail.com>
2018-10-05 14:09:20 +1000

    Updates

---
+++ precache-manifest.9461f1ec70750cdefe82cb5fee1d8654.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca8b6bf5ce3df85a66ee",
+    "url": "/mudaligekosala/appcy/static/js/runtime~main.ca8b6bf5.js"
+  },
+  {
+    "revision": "b854d6a07c94d1ef0a09",
+    "url": "/mudaligekosala/appcy/static/js/main.b854d6a0.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/mudaligekosala/appcy/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "3781c6557b866a6877f5dece91d31ba7",
+    "url": "/mudaligekosala/appcy/index.html"
+  }
+];
